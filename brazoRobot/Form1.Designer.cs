ea6c9581c8cb30b11df2c9db3899550a51bed2e6
EVERTH DAVID CUBIDES ENCISO <fibonacci1235@hotmail.com>
2020-03-28 13:32:36 -0500

    priemr commit

---
+++ brazoRobot/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace brazoRobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEjecinco = new System.Windows.Forms.TextBox();
+            this.txtEjecuatro = new System.Windows.Forms.TextBox();
+            this.txtEjetres = new System.Windows.Forms.TextBox();
+            this.txtEjedos = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEjeuno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnEjeunomas = new System.Windows.Forms.Button();
+            this.btnEjeunomenos = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.btnEjeunomenos);
+            this.groupBox1.Controls.Add(this.btnEjeunomas);
+            this.groupBox1.Controls.Add(this.txtEjecinco);
+            this.groupBox1.Controls.Add(this.txtEjecuatro);
+            this.groupBox1.Controls.Add(this.txtEjetres);
+            this.groupBox1.Controls.Add(this.txtEjedos);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtEjeuno);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control Brazo";
+            // 
+            // txtEjecinco
+            // 
+            this.txtEjecinco.Location = new System.Drawing.Point(73, 160);
+            this.txtEjecinco.Name = "txtEjecinco";
+            this.txtEjecinco.Size = new System.Drawing.Size(77, 20);
+            this.txtEjecinco.TabIndex = 9;
+            // 
+            // txtEjecuatro
+            // 
+            this.txtEjecuatro.Location = new System.Drawing.Point(73, 128);
+            this.txtEjecuatro.Name = "txtEjecuatro";
+            this.txtEjecuatro.Size = new System.Drawing.Size(77, 20);
+            this.txtEjecuatro.TabIndex = 8;
+            // 
+            // txtEjetres
+            // 
+            this.txtEjetres.Location = new System.Drawing.Point(73, 93);
+            this.txtEjetres.Name = "txtEjetres";
+            this.txtEjetres.Size = new System.Drawing.Size(77, 20);
+            this.txtEjetres.TabIndex = 7;
+            // 
+            // txtEjedos
+            // 
+            this.txtEjedos.Location = new System.Drawing.Point(73, 56);
+            this.txtEjedos.Name = "txtEjedos";
+            this.txtEjedos.Size = new System.Drawing.Size(77, 20);
+            this.txtEjedos.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Eje 5:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Eje 4:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Eje 3:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Eje 2:";
+            // 
+            // txtEjeuno
+            // 
+            this.txtEjeuno.Location = new System.Drawing.Point(73, 25);
+            this.txtEjeuno.Name = "txtEjeuno";
+            this.txtEjeuno.Size = new System.Drawing.Size(77, 20);
+            this.txtEjeuno.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eje 1:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(448, 425);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // btnEjeunomas
+            // 
+            this.btnEjeunomas.Location = new System.Drawing.Point(166, 25);
+            this.btnEjeunomas.Name = "btnEjeunomas";
+            this.btnEjeunomas.Size = new System.Drawing.Size(54, 28);
+            this.btnEjeunomas.TabIndex = 10;
+            this.btnEjeunomas.Text = "+";
+            this.btnEjeunomas.UseVisualStyleBackColor = true;
+            this.btnEjeunomas.Click += new System.EventHandler(this.btnEjeunomas_Click);
+            // 
+            // btnEjeunomenos
+            // 
+            this.btnEjeunomenos.Location = new System.Drawing.Point(226, 25);
+            this.btnEjeunomenos.Name = "btnEjeunomenos";
+            this.btnEjeunomenos.Size = new System.Drawing.Size(54, 28);
+            this.btnEjeunomenos.TabIndex = 11;
+            this.btnEjeunomenos.Text = "-";
+            this.btnEjeunomenos.UseVisualStyleBackColor = true;
+            this.btnEjeunomenos.Click += new System.EventHandler(this.btnEjeunomenos_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(83, 231);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(83, 268);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 13;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 307);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(251, 45);
+            this.trackBar1.TabIndex = 14;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Brazo Robot";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEjeuno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEjecinco;
+        private System.Windows.Forms.TextBox txtEjecuatro;
+        private System.Windows.Forms.TextBox txtEjetres;
+        private System.Windows.Forms.TextBox txtEjedos;
+        private System.Windows.Forms.Button btnEjeunomenos;
+        private System.Windows.Forms.Button btnEjeunomas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ brazoRobot/Form1.cs
@@ -0,0 +1,160 @@
+﻿using brazoRobot.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace brazoRobot
+{
+    public partial class Form1 : Form
+    {
+        brazoUno brazouno;
+
+        Bitmap bmp = new Bitmap(400, 400);
+        Graphics g;
+        int r = 200 / 2 - 70;
+        int l = 10 * 5;
+        int angle = 0;
+        int center = 400 / 2;
+        int R;
+        int accuracy = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            //dibujar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bmp); // Binds the graphics to the Bitmap
+            pictureBox1.Image = bmp;
+            R = (int)Math.Sqrt((Math.Pow(l / 2, 2) + Math.Pow(r, 2))); // Calculate the bigger radius
+            Render();
+        }
+
+
+
+        private void dibujar()
+        {
+            brazouno = new brazoUno(pictureBox1, txtEjeuno);
+            brazouno.graficarBrazo();
+        }
+
+        private void btnEjeunomas_Click(object sender, EventArgs e)
+        {
+            brazouno.graficarBrazo();
+        }
+
+        private void btnEjeunomenos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int xcentro = pictureBox1.Width / 2;
+            int ycentro = pictureBox1.Height / 2;
+
+            Pen lapiz = new Pen(Color.Black, 2);
+
+            e.Graphics.TranslateTransform(xcentro, ycentro);
+            e.Graphics.ScaleTransform(1, -1); //convierte a coordenadas normales
+
+            //dibujar ejes x y y 
+            //linea horizontal
+            e.Graphics.DrawLine(lapiz, xcentro * -1, 0, xcentro * 2, 0); //eje x
+            //linea vertical
+            e.Graphics.DrawLine(lapiz, 0, ycentro, 0, ycentro * -1); //eje y
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            l = (int)numericUpDown1.Value * 5; // As soon as the Value Changed store it in the l int
+            Render(); // Execute render function
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            r = 400 / 4 + ((int)numericUpDown2.Value * 5);
+            Render();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            angle = trackBar1.Value; // As soon as the Value Changed store it in the angle int
+            Render(); // Execute render function
+        }
+
+        private int[] LineCoord(int angleIn, int radius, int center) // Get any point on the circle by the angle
+        {
+            int[] coord = new int[2]; // Setting up the int array for return
+            angleIn %= (360 * accuracy);
+            angleIn *= 1;
+
+            if (angleIn >= 0 && angleIn <= (180 * accuracy))
+            {
+                coord[0] = center + (int)(radius * Math.Sin(Math.PI * angleIn / (180 * accuracy)));
+                coord[1] = center - (int)(radius * Math.Cos(Math.PI * angleIn / (180 * accuracy)));
+            }
+            else
+            {
+                coord[0] = center - (int)(radius * -Math.Sin(Math.PI * angleIn / (180 * accuracy)));
+                coord[1] = center - (int)(radius * Math.Cos(Math.PI * angleIn / (180 * accuracy)));
+            }
+            return coord;
+        }
+
+        // These functions converts the Degrees of an angle to Radians and v.v
+        double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        double RadianToDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
+        private void Render()
+        {
+              g = Graphics.FromImage(bmp); // Binds the graphics to the Bitmap
+           // g = pictureBox1.CreateGraphics();
+            pictureBox1.Image = bmp;
+            g.Clear(Color.White); // Clears the screen
+
+            R = (int)Math.Sqrt((Math.Pow((l / 2), 2) + Math.Pow(r, 2))); // Calculate the bigger radius
+            int theta = (int)RadianToDegree(Math.Atan((double)(l / 2) / r)); // Calculate the angle theta
+
+            // This calculates the bigger radius' lines' coordinates
+            int x0 = LineCoord(angle + theta, R, center)[0];
+            int y0 = LineCoord(angle + theta, R, center)[1];
+            int x1 = LineCoord(angle + 360 - theta, R, center)[0];
+            int y1 = LineCoord(angle + 360 - theta, R, center)[1];
+
+         
+           
+                //liena principal
+                g.DrawLine(new Pen(Color.FromArgb(150, 0, 0), 50f), new Point(center, center*2), new Point(LineCoord(angle, r, center)[0], LineCoord(angle, r, center)[1])); // Draw the Handle
+            
+            //liena azul
+              // g.DrawLine(new Pen(Color.FromArgb(0, 0, 230), 4f), new Point(x0, y0), new Point(x1, y1)); // Draw the Perpendicular Line
+           
+            //lineas a los lados
+                //g.DrawLine(new Pen(Color.FromArgb(100, 100, 0), 1f), new Point(center, center), new Point(x0, y0)); // From centar to points on the Perpendicular Line
+                //g.DrawLine(new Pen(Color.FromArgb(100, 100, 0), 1f), new Point(center, center), new Point(x1, y1)); // From centar to points on the Perpendicular Line
+            
+
+            g.Dispose();
+        }
+
+
+
+    }
+}
